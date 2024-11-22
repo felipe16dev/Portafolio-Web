6839a96231b51429ae17109fe50328f57790d1b6
Carlos Felipe Corrales <carlosf9916corr@gmail.com>
2024-11-21 19:13:04 -0500

    Formulario Contacto funcional

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+/* =============== FORMULARIO CONTACTO =============== */
+const $form = document.querySelector('#form');
+
+$form.addEventListener('submit',handleSubmit);
+
+async function handleSubmit(e){
+    e.preventDefault();
+
+    const form = new FormData(this);
+    const response = await fetch(this.action,{
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if(response.ok){
+        this.reset()
+        alert('Gracias por contactarme, te escribiré pronto :D')
+    }
+}
